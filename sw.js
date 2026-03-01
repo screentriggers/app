fe44c81ca933e7bddefe362961e357fb9b96d810
screentriggers <agalea.xo@gmail.com>
2026-03-01 16:37:00 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,43 @@
-self.addEventListener('install', function(e) {
+const CACHE_NAME = "screen-triggers-v2";
+
+const urlsToCache = [
+  "/app/",
+  "/app/index.html",
+  "/app/manifest.json",
+  "/app/icon-192.png",
+  "/app/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
