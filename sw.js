7887103b7c98ca2294f33d38ec561e6ecb269c7e
screentriggers <agalea.xo@gmail.com>
2026-03-01 15:10:23 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,21 @@
-const CACHE_NAME = "screen-triggers-v1";
+const CACHE = "screen-triggers-v1";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
