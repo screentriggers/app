5733c335bbd7b63f1444729779ba5e6d43cff3f8
screentriggers <agalea.xo@gmail.com>
2026-03-01 16:04:35 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,38 +10,29 @@ const urlsToCache = [
   "/app/icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", event => {
   self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys.filter(key => key !== CACHE_NAME)
             .map(key => caches.delete(key))
-      );
-    })
+      )
+    )
   );
-
   self.clients.claim();
 });
 
-// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
