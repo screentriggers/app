0691851abbadbd3820a9dd7801fb7c69c417f52e
screentriggers <agalea.xo@gmail.com>
2026-03-03 17:31:59 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "screen-triggers-v1";
+const URLS_TO_CACHE = [
+  "/app/",
+  "/app/index.html",
+  "/app/manifest.json",
+  "/app/icon-192.png",
+  "/app/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
